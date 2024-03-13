78d3b4037b628ab9f2fe8132a2a2ade1f7e85a06
Ajay Pratap <singhajaypratap839@gmail.com>
2024-03-13 19:19:47 +0530

    Code-base-commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
+function App() {
+  return (
+    <>
+     <Navbar title="TextUtils" aboutText="About"/>
+     <div className="container">
+       <TextForm heading= "Enter The Text to analyse"/>
+     </div>
+    </>
+  );
+}
+
+export default App;
